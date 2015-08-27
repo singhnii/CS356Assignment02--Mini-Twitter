5b70890c929729c385b24183316ee4d71cf62fa9
singhnii <singhnii@users.noreply.github.com>
2015-08-27 07:05:19 +0000

    Delete Component.java

--- Component.java
+++
@@ -1,8 +0,0 @@
-
-//Component (for the composite pattern)
-
-import javax.swing.tree.DefaultMutableTreeNode;
-
-public interface Component {
-	//public void add(DefaultMutableTreeNode node, String id);
-}
